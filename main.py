f0d3f2af3e0330879ec14731fc175a23de973024
zhangjunda23 <zhangjunda23@gmail.com>
2022-05-11 18:50:28 +0800

    完成了HMC、MH两种方法的采样和显示

--- main.py
+++ main.py
@@ -1,37 +1,33 @@
 import math
 import random
 
+import numpy
+from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-# 势能
-from matplotlib.animation import FuncAnimation
+# 用HMC采样
+def HMC():
+    # 势能
+    def U(xx: np.ndarray):
+        res = np.matmul(xx, np.linalg.inv(np.array([[1, 0.8], [0.8, 1]])))
+        res = np.matmul(res, np.transpose(xx))
+        return res
 
+    # 势能的导数
+    def dU(xx: np.ndarray):
+        res = np.matmul(xx, np.linalg.inv(np.array([[1, 0.8], [0.8, 1]])))
+        return res
 
-def U(xx: np.ndarray):
-    res = np.matmul(xx, np.linalg.inv(np.array([[1, 0.8], [0.8, 1]])))
-    res = np.matmul(res, np.transpose(xx))
-    return res
+    # 动能
+    def K(p: np.ndarray):
+        res = np.matmul(p, np.transpose(p))
+        return res
 
-
-# 势能的导数
-def dU(xx: np.ndarray):
-    res = np.matmul(xx, np.linalg.inv(np.array([[1, 0.8], [0.8, 1]])))
-    return res
-
-
-# 动能
-def K(p: np.ndarray):
-    res = np.matmul(p, np.transpose(p))
-    return res
-
-
-if __name__ == '__main__':
-    np.random.seed(1234)
     delta = 0.3  # leap frog的步长
-    nSample = 200  # 需要采样的样本数量
-    L = 20  # leap frog的步数
+    nSample = 50000  # 需要采样的样本数量
+    L = 20  # leap frog的步数，步数越多，收敛得越快
 
     # 初始化
     x = np.zeros((nSample, 2))
@@ -62,20 +58,96 @@ if __name__ == '__main__':
         u = random.random()
         if u < alpha:
             x[t, :] = xStar
-            print(xStar)
         else:
             x[t, :] = x[t - 1, :]
 
     fig, ax = plt.subplots()
-    line, = plt.plot(x[:, 0], x[:, 1], 'ob-')
+    line, = plt.plot(x[:, 0], x[:, 1], 'o', linewidth=0.5, markersize=1)
+    ax.set_xlim(-10, 10)
+    ax.set_ylim(-10, 10)
 
-
-    def update(i):
-        line.set_xdata(x[0:i, 0])
-        line.set_ydata(x[0:i, 1])
-        return line
-
-
-    ani = FuncAnimation(fig, update, interval=100)
+    # def update(i):
+    #     global repeatCount
+    #     global validCount
+    #     line.set_xdata(x[0:i, 0])
+    #     line.set_ydata(x[0:i, 1])
+    #     if i > 0:
+    #         if (x[i, :] == x[i - 1, :]).all():
+    #             repeatCount = repeatCount + 1
+    #         else:
+    #             validCount = validCount + 1
+    #     fig.suptitle("%d个有效点，%d个重复点" % (validCount, repeatCount))
+    #     return line, fig
+    #
+    # ani = FuncAnimation(fig, update, interval=100)
 
     plt.show()
+    plt.pause(0)
+
+
+# 用metropolis采样方法
+def MH():
+    # 二维正态分布求点xx处的概率
+    def TargetDis(xx):
+        sigma = np.array([[1, 0.8], [0.8, 1]])
+        c = 1 / (math.sqrt(2 * math.pi * np.linalg.det(sigma)))
+        m = np.matmul(xx, np.linalg.inv(sigma))
+        m = np.matmul(m, np.transpose(xx))
+        res = c * math.exp(-1 / 2 * m)
+        return res
+
+    # 初始化
+    nSample = 50000  # 需要采样的样本数量
+    x = np.zeros((nSample, 2))
+    x0 = np.array([0, 6])
+    x[0, :] = x0
+    alpha = 0
+    currentP = TargetDis(x0)
+    t = 0
+
+    while t < nSample - 1:
+        t = t + 1
+        point = np.random.rand(1, 2) * 20 - 10  # 在[-5,5][-5,5]范围内均匀采样一个点
+        p = TargetDis(point)
+        alpha = min(1., p / currentP)
+        u = random.random()
+        if u < alpha:
+            x[t, :] = point
+            currentP = p
+        else:
+            x[t, :] = x[t - 1, :]
+
+    fig, ax = plt.subplots()
+    # line, = plt.plot(x[0, 0], x[0, 1], 'o', linewidth=1, markersize=1)
+    line, = plt.plot(x[:, 0], x[:, 1], 'o', linewidth=1, markersize=1)
+    ax.set_xlim(-10, 10)
+    ax.set_ylim(-10, 10)
+
+    def update(i):
+        global repeatCount
+        global validCount
+        line.set_xdata(x[0:i, 0])
+        line.set_ydata(x[0:i, 1])
+        if i > 0:
+            if (x[i, :] == x[i - 1, :]).all():
+                repeatCount = repeatCount + 1
+            else:
+                validCount = validCount + 1
+        fig.suptitle("%d个有效点，%d个重复点" % (validCount, repeatCount))
+        return line, fig
+
+    ani = FuncAnimation(fig, update, interval=0.01)
+
+    plt.show()
+    plt.pause(0)
+
+
+if __name__ == '__main__':
+    plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
+    plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
+    np.random.seed(1234)
+
+    repeatCount = 0
+    validCount = 0
+    HMC()
+    # MH()
