574c8f98b36c68e7c38b12c450bed3e2abb631d5
zhangjunda23 <zhangjunda23@gmail.com>
2022-05-13 18:55:13 +0800

    增加了进度条

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import math
 import random
-
-import numpy
+import tqdm
 from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,13 +8,14 @@ import numpy as np
 
 # 用HMC采样
 def HMC():
-    # 势能
+    # 势能，常数被忽略
     def U(xx: np.ndarray):
         res = np.matmul(xx, np.linalg.inv(np.array([[1, 0.8], [0.8, 1]])))
         res = np.matmul(res, np.transpose(xx))
         return res
 
-    # 势能的导数
+    # 势能的导数，常数被忽略。这里是求地偏导向量，即[\frac{\partial U}{x_1},\frac{\partial U}{x_2}]。太跳跃了，因为\Sigma是对称矩阵
+    # 所以其逆矩阵也是对称矩阵，计算出来就有了dU
     def dU(xx: np.ndarray):
         res = np.matmul(xx, np.linalg.inv(np.array([[1, 0.8], [0.8, 1]])))
         return res
@@ -35,32 +35,36 @@ def HMC():
 
     x[0, :] = x0
     t = 0
+    with tqdm.tqdm(total=nSample) as pbar:
+        pbar.set_description('采样进度:')
+        pbar.update(1)
+        while t < nSample - 1:
+            t = t + 1
+            p0 = np.random.rand(1, 2)  # 随机采样一个动量
+            # leap frog方法
+            pStar = p0 - delta / 2 * dU(x[t - 1, :])  # 动量移动半步
+            xStar = x[t - 1, :] + delta * pStar  # 位置移动一步，这里pStar是U对P的偏导
+            for jL in range(1, L - 1):
+                pStar = pStar - delta * dU(xStar)
+                xStar = xStar + delta * pStar
+            pStar = pStar - delta / 2 * dU(xStar)
 
-    while t < nSample - 1:
-        t = t + 1
-        p0 = np.random.rand(1, 2)  # 随机采样一个动量
-        # leap frog方法
-        pStar = p0 - delta / 2 * dU(x[t - 1, :])  # 动量移动半步
-        xStar = x[t - 1, :] + delta * pStar  # 位置移动一步，这里pStar是U对P的偏导
-        for jL in range(1, L - 1):
-            pStar = pStar - delta * dU(xStar)
-            xStar = xStar + delta * pStar
-        pStar = pStar - delta / 2 * dU(xStar)
+            U0 = U(x[t - 1, :])
+            UStar = U(xStar)[0, 0]
 
-        U0 = U(x[t - 1, :])
-        UStar = U(xStar)[0, 0]
+            K0 = K(p0)
+            KStar = K(pStar)[0, 0]
 
-        K0 = K(p0)
-        KStar = K(pStar)[0, 0]
+            # 计算是否接受这个样本
+            alpha = min(1., math.exp((U0 + K0) - (UStar + KStar)))
+            u = random.random()
+            if u < alpha:
+                x[t, :] = xStar
+            else:
+                x[t, :] = x[t - 1, :]
+            pbar.update(1)
 
-        # 计算是否接受这个样本
-        alpha = min(1., math.exp((U0 + K0) - (UStar + KStar)))
-        u = random.random()
-        if u < alpha:
-            x[t, :] = xStar
-        else:
-            x[t, :] = x[t - 1, :]
-
+    # 画图
     fig, ax = plt.subplots()
     line, = plt.plot(x[:, 0], x[:, 1], 'o', linewidth=0.5, markersize=1)
     ax.set_xlim(-10, 10)
@@ -104,18 +108,21 @@ def MH():
     alpha = 0
     currentP = TargetDis(x0)
     t = 0
-
-    while t < nSample - 1:
-        t = t + 1
-        point = np.random.rand(1, 2) * 20 - 10  # 在[-5,5][-5,5]范围内均匀采样一个点
-        p = TargetDis(point)
-        alpha = min(1., p / currentP)
-        u = random.random()
-        if u < alpha:
-            x[t, :] = point
-            currentP = p
-        else:
-            x[t, :] = x[t - 1, :]
+    with tqdm.tqdm(total=nSample) as pbar:
+        pbar.set_description('采样进度:')
+        pbar.update(1)
+        while t < nSample - 1:
+            t = t + 1
+            point = np.random.rand(1, 2) * 20 - 10  # 在[-5,5][-5,5]范围内均匀采样一个点
+            p = TargetDis(point)
+            alpha = min(1., p / currentP)
+            u = random.random()
+            if u < alpha:
+                x[t, :] = point
+                currentP = p
+            else:
+                x[t, :] = x[t - 1, :]
+            pbar.update(1)
 
     fig, ax = plt.subplots()
     # line, = plt.plot(x[0, 0], x[0, 1], 'o', linewidth=1, markersize=1)
